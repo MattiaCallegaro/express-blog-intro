1211e51c05134a2e3d397fc4743dad8942c31256
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-16 15:44:28 +0200

    [UPD] push for ticket

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-//importazione di express
+// 1 importazione di express
 const express = require("express");
 
-//inizializzo express nella variabile app
+// 2 inizializzo express nella variabile app
 const app = express();
 
-//definisco il numero della porta
+// 3 definisco il numero della porta
 const port = 3000;
 
-//definisco la rotta base
+// 4 definisco la rotta base e con <res.send> invio la risposta ("Server del mio blog") al server
 app.get('/', (req, res)=>{
     res.send("Server del mio blog")
 })
 
-//faccio restare in ascolto il server
+// 5 faccio restare in ascolto il server per visualizzare su postman
 app.listen(port, ()=>{
     console.log(`Server del blog in ascolto alla porta ${port}`)
 })
+
+// 6 imposto lo strumento per recuperare gli asset statici usando .use e .static nella cartella public
+app.use(express.static ('public'))
