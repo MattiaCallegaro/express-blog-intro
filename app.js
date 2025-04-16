d2d4a676023375f2a24719fe311c1027e108d476
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-04-16 15:25:06 +0200

    [UPD] create base server path

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+//importazione di express
+const express = require("express");
+
+//inizializzo express nella variabile app
+const app = express();
+
+//definisco il numero della porta
+const port = 3000;
+
+//definisco la rotta base
+app.get('/', (req, res)=>{
+    res.send("Server del mio blog")
+})
+
+//faccio restare in ascolto il server
+app.listen(port, ()=>{
+    console.log(`Server del blog in ascolto alla porta ${port}`)
+})
